35d69f67efd751a76a2f440e61f61a7349392d22
Marcel Hekking <marcelhekking@gmail.com>
2025-03-26 08:58:28 +0100

    Introduce `domain_name` again

--- {{cookiecutter.project_slug}}/tests/models/test_users.py
+++ {{cookiecutter.project_slug}}/tests/models/test_users.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 
 @pytest.mark.django_db
 def test_create_user():
-    user = User.objects.create_user("test123", "user@{{cookiecutter.project_slug}}.nl")
+    user = User.objects.create_user("test123", "user@{{cookiecutter.domain_name}}")
     assert user.pk
-    assert user.email == "user@{{cookiecutter.project_slug}}.nl"
+    assert user.email == "user@{{cookiecutter.domain_name}}"
     assert not user.is_staff
     assert not user.is_superuser
